704c47eb13df226291970b1ebde013757e1f4006
nasiabuslikprog <nasiabuslikprog@yandex.ru>
2022-10-20 11:21:55 +0300

    Changed the names of variables in the service class

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
-    public int calculate(int a) {
-        int bonus = a / 20;
-        return bonus;
+    public int calculate(int amountBuy) {
+        int bonusMiles = amountBuy / 20;
+        return bonusMiles;
     }
 
 }
